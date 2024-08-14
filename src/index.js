7e6ed815b992ea428b710d84f2050af4346e1862
29Jeanca <jeancarlosbarberena29@gmail.com>
2024-08-13 22:04:16 -0600

    a

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
-// Inserte el código aquí
+import { calculadora } from './calculadora';
 
+function calcular(operacion, numero1, numero2) {
+  switch (operacion) {
+    case 'sumar':
+      return calculadora.sumar(numero1, numero2);
+    case 'restar':
+      return calculadora.restar(numero1, numero2);
+    case 'dividir':
+      return calculadora.dividir(numero1, numero2);
+    case 'multiplicar':
+      return calculadora.multiplicar(numero1, numero2);
+    default:
+      throw new Error('Operación no válida');
+  }
+}
