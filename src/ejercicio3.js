5a75def6a9fe4dae18d058f700535fc08f837aee
29Jeanca <jeancarlosbarberena29@gmail.com>
2024-08-13 21:58:07 -0600

    a

--- src/ejercicio3.js
+++ src/ejercicio3.js
@@ -1,6 +1,18 @@
 // Inserte el código aquí
-
-
+function clonarArreglo(arreglo) {
+    return [...arreglo]; 
+  }
+  function clonarObjeto(objeto) {
+    return { ...objeto }; 
+  }
+  function sumar(...numeros) {
+    return numeros.reduce((acumulador, valorActual) => acumulador + valorActual, 0);
+  }
+  
+  function saludar(nombre = "Desconocido") {
+    return `Hola ${nombre}`;
+  }
+  
 
 // NO MODIFICAR
 export { clonarArreglo, clonarObjeto, sumar, saludar };
